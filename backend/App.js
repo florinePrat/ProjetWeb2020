4108d630d3ba64cafcf8f382b1621f2a37ee04c3
florine <florinepltg64@gmail>
2020-01-14 11:46:26 +0100

    delete stuff's things to keep auth

--- backend/App.js
+++ backend/App.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
-const path = require('path');
 const app = express();
 
 mongoose.connect('mongodb+srv://Florine:florine@cluster0-hqmob.mongodb.net/test?retryWrites=true&w=majority',
@@ -21,9 +19,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
-app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 module.exports = app;
