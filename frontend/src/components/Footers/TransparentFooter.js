9e7630665ec5cbae6da1870a0b98d75a273347e1
florine <florinepltg64@gmail>
2020-03-12 10:49:11 +0100

    delete template redirection on footer

--- frontend/src/components/Footers/TransparentFooter.js
+++ frontend/src/components/Footers/TransparentFooter.js
@@ -12,42 +12,20 @@ function TransparentFooter() {
           <ul>
             <li>
               <a
-                href="https://www.creative-tim.com?ref=nukr-transparent-footer"
-                target="_blank"
+                  href="./landing-page"
+                  target="_blank"
               >
                 Accueil
-              </a>
-            </li>
-            <li>
-              <a
-                href="http://presentation.creative-tim.com?ref=nukr-transparent-footer"
-                target="_blank"
-              >
-                En savoir plus
-              </a>
-            </li>
-            <li>
-              <a
-                href="http://blog.creative-tim.com?ref=nukr-transparent-footer"
-                target="_blank"
-              >
-                Rechercher une salle
               </a>
             </li>
           </ul>
         </nav>
         <div className="copyright" id="copyright">
-          © {new Date().getFullYear()}, Designed by{" "}
-          <a
-            href="https://www.invisionapp.com?ref=nukr-transparent-footer"
-            target="_blank"
-          >
-            Invision
-          </a>
+          © {new Date().getFullYear()}
           . Coded by{" "}
           <a
-            href="https://www.creative-tim.com?ref=nukr-transparent-footer"
-            target="_blank"
+              href="./landing-page"
+              target="_blank"
           >
             Locat'me
           </a>
